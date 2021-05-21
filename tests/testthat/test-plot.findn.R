be7bafeed1f2c52b973c8ab7dd8705cda17d8c4c
lbau7 <l.baumann@aon.at>
2021-05-21 10:45:42 +0200

    add tests

--- tests/testthat/test-plot.findn.R
+++ tests/testthat/test-plot.findn.R
@@ -1,14 +1,23 @@
 test_that("plot.findn works", {
   set.seed(20210520)
-  fun_ttest <- function(n, k) {
+  fun_ttest1 <- function(n, k) {
     n_mat <- matrix(rnorm(n * k, mean = 5, sd = 10), ncol = k)
     pvals <- apply(n_mat, 2, function(x) t.test(x)$p.value)
     mean(pvals <= 0.05)
   }
   
-  res_bll <- suppressWarnings(findn(fun = fun_ttest, targ = 0.8, start = 100))
-  plot_bll1 <- plot(res_bll, min_n = 1, power_lim = 0.95)
-  plot_bll2 <- plot(res_bll, min_n = 1, max_n = 100)
+  fun_ttest2 <- function(n, k) {
+    n_mat <- matrix(rnorm(n * k, mean = 5, sd = 2.5), ncol = k)
+    pvals <- apply(n_mat, 2, function(x) t.test(x)$p.value)
+    mean(pvals <= 0.05)
+  }
+  
+  res_bll1 <- suppressWarnings(findn(fun = fun_ttest1, targ = 0.8, start = 100))
+  plot_bll1 <- plot(res_bll1, min_n = 1, power_lim = 0.95)
+  plot_bll2 <- plot(res_bll1, min_n = 1, max_n = 100)
+  res_bll2 <- suppressWarnings(findn(fun = fun_ttest2, targ = 0.8, start = 100,
+    max_evals = 3000))
+  plot_bll3 <- plot(res_bll2)
   
   expect_is(plot_bll1, "ggplot")
   expect_gt(max(plot_bll1$data$Lower.CL), 0.95)
@@ -18,6 +27,7 @@ test_that("plot.findn works", {
     plot_bll1$data$Rating == "Sufficient")] > 0.8))
   expect_equal(max(plot_bll2$data$n), 100)
   expect_equal(min(plot_bll2$data$n), 1)
+  expect_equal(sum(plot_bll3$data$Rating == "Uncertain"), 0)
 })
 
   
--- tests/testthat/test-print.findn.R
+++ tests/testthat/test-print.findn.R
@@ -1,27 +1,38 @@
 test_that("plot.findn works", {
   set.seed(20210520)
-  fun_ttest <- function(n, k) {
+  fun_ttest1 <- function(n, k) {
     n_mat <- matrix(rnorm(n * k, mean = 5, sd = 10), ncol = k)
     pvals <- apply(n_mat, 2, function(x) t.test(x)$p.value)
     mean(pvals <= 0.05)
   }
   
-  res_bll <- suppressWarnings(findn(fun = fun_ttest, targ = 0.8, start = 100))
-  det_bll1 <- print(res_bll, details = "low", invisible = TRUE)
-  pred_bll <- predict_fit(res_bll$fit, det_bll1$Minimum_Sufficient_n, se = TRUE)
-  low_cl <- stats::pnorm(pred_bll$pred - stats::qnorm(1 - (res_bll$level / 2)) * 
-      pred_bll$se)
-  det_bll2 <- print(res_bll, details = "high", max_n = 10, invisible = TRUE)
-  det_bll3 <- print(res_bll, details = "high", invisible = TRUE)
+  fun_ttest2 <- function(n, k) {
+    n_mat <- matrix(rnorm(n * k, mean = 5, sd = 2.5), ncol = k)
+    pvals <- apply(n_mat, 2, function(x) t.test(x)$p.value)
+    mean(pvals <= 0.05)
+  }
+  
+  res_bll1 <- suppressWarnings(findn(fun = fun_ttest1, targ = 0.8, start = 100))
+  det_bll11 <- print(res_bll1, details = "low", invisible = TRUE)
+  pred_bll <- predict_fit(res_bll1$fit, det_bll11$Minimum_Sufficient_n, 
+    se = TRUE)
+  low_cl <- stats::pnorm(pred_bll$pred - stats::qnorm(1 - (res_bll1$level / 
+      2)) * pred_bll$se)
+  det_bll12 <- print(res_bll1, details = "high", max_n = 10, invisible = TRUE)
+  det_bll13 <- print(res_bll1, details = "high", invisible = TRUE)
+  res_bll2 <- suppressWarnings(findn(fun = fun_ttest2, targ = 0.8, start = 100,
+    max_evals = 3000))
+  det_bll2 <- print(res_bll2, details = "high", invisible = TRUE)
     
   expect_gt(low_cl, 0.8)
-  expect_equal(nrow(det_bll2$Details), 10)
-  expect_equal(sum(det_bll3$Details$Rating == "Too Low"), 3)
-  expect_equal(sum(det_bll3$Details$Rating == "Sufficient"), 3)
-  expect_lt(max(as.numeric(det_bll3$Details$Upper.CL[which(
-    det_bll3$Details$Rating == "Too Low")])), 0.8)
-  expect_gt(max(as.numeric(det_bll3$Details$Lower.CL[which(
-    det_bll3$Details$Rating == "Sufficient")])), 0.8)
+  expect_equal(nrow(det_bll12$Details), 10)
+  expect_equal(sum(det_bll13$Details$Rating == "Too Low"), 3)
+  expect_equal(sum(det_bll13$Details$Rating == "Sufficient"), 3)
+  expect_lt(max(as.numeric(det_bll13$Details$Upper.CL[which(
+    det_bll13$Details$Rating == "Too Low")])), 0.8)
+  expect_gt(max(as.numeric(det_bll13$Details$Lower.CL[which(
+    det_bll13$Details$Rating == "Sufficient")])), 0.8)
+  expect_equal(sum(det_bll2$Details$Rating == "Uncertain"), 0)
 })
 
   
